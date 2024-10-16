a919b45e27e9003ee675e22e644fa33f89d3d193
lee <likachich12@gmail.com>
2024-10-16 13:05:29 +0300

    two functions added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+int **createMatrix(size_t m, size_t n);
+void dt(int **t, size_t m, size_t n);
 int main() {
   size_t n = 0, m = 0;
   std::cin >> m >> n;
-  std::cout << m << n;
+  int **t = createMatrix(m, n);
+  dt(t, m, n);
 }
